df98cec044eb2364457951cd89ffe563fa19418b
effective-pome <xxyusuxx1011@gmail.com>
2026-01-16 01:34:29 +0900

    Update GAS_URL to new deployment URL

--- config.js
+++ config.js
@@ -8,7 +8,7 @@
 window.CONFIG = {
     // Google Apps Script Web App URL
     // GASをデプロイ後、ここにURLを設定してください
-    GAS_URL: 'https://script.google.com/macros/s/AKfycbzPFHXPpjYhDpL--2_NIHbpejCMqSRr974zwFl2NkDpYgQYG_Kg6ZQNcXBgGrztCYmdSA/execc',
+    GAS_URL: 'https://script.google.com/macros/s/AKfycbz_v-ryvOez8fxin6WSaCRTLhGjcYo_pQ4LlZWENu1StBni9P2ruxM1gqdQrni1ZxFZ/exec',
 
     // Hot Pepper Beauty Review URL (Primary)
     // サロンのHot Pepper Beauty口コミ投稿ページURLを設定してください
